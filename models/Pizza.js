490bbf97134326c8fded0c58195acf7fffede15b
spaghettimedic <dsteiner247@gmail.com>
2022-02-01 12:28:50 -0500

    installed: MongoDB, Mongoose, and npm packages; created Pizza Model, Controller, and API routes; tested API and connected front-end form

---
+++ models/Pizza.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  crust: {
+    type: String,
+    default: 'Hand-Tossed'
+  },
+  toppings: []
+});
+
+// create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
