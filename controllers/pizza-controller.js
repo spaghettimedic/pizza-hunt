4e3bac972b871888fb91b6a823a7ec5ffd1f04d9
spaghettimedic <dsteiner247@gmail.com>
2022-02-07 19:29:32 -0500

    created the Comment model, associated Pizza and Comment models, created Pizza controller methods, created and tested Comment routes, implemented dateFormat() getter

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,6 +4,12 @@ const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
+    .sort({ _id: -1 })
     .then(dbPizzaData => res.json(dbPizzaData))
     .catch(err => {
       console.log(err);
@@ -14,6 +20,11 @@ const pizzaController = {
   // get one pizza by id
   getPizzaById({ params}, res) {
     Pizza.findOne({ _id: params.id })
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
     .then(dbPizzaData => {
       // if no pizza is found, send 404
       if (!dbPizzaData) {
@@ -37,7 +48,11 @@ const pizzaController = {
 
   // update pizza by id
   updatePizza({ params, body }, res) {
-    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+    Pizza.findOneAndUpdate(
+      { _id: params.id },
+      body,
+      { new: true }
+    )
     .then(dbPizzaData => {
       if (!dbPizzaData) {
         res.status(404).json({ message: 'No pizza found with this id!' });
