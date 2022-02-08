0d0537f15637c9e769fb5c2f833efb873df5c07c
spaghettimedic <dsteiner247@gmail.com>
2022-02-08 09:45:32 -0500

    set up front-end comment functionality, created comment reply schema, updated comment model's controller and routes

--- public/assets/js/single-pizza.js
+++ public/assets/js/single-pizza.js
@@ -9,6 +9,28 @@ const $newCommentForm = document.querySelector('#new-comment-form');
 
 let pizzaId;
 
+function getPizza() {
+  // get id of pizza
+  const searchParams = new URLSearchParams(document.location.search.substring(1));
+  const pizzaId = searchParams.get('id');
+
+  // get pizzaInfo
+  fetch(`/api/pizzas/${pizzaId}`)
+  .then(response => {
+    // check for a 400 or 500 error from server
+    if (!response.ok) {
+      throw new Error({ message: 'Something went wrong!' });
+    }
+    return response.json();
+  })
+  .then(printPizza)
+  .catch(err => {
+    console.log(err);
+    alert('Cannot find a pizza with this id! Taking you back.');
+    window.history.back();
+  });
+};
+
 function printPizza(pizzaData) {
   console.log(pizzaData);
 
@@ -29,7 +51,7 @@ function printPizza(pizzaData) {
   } else {
     $commentSection.innerHTML = '<h4 class="bg-dark p-3 rounded">No comments yet!</h4>';
   }
-}
+};
 
 function printComment(comment) {
   // make div to hold comment and subcomments
@@ -74,7 +96,7 @@ function printReply(reply) {
     <p>${reply.replyBody}</p>
   </div>
 `;
-}
+};
 
 function handleNewCommentSubmit(event) {
   event.preventDefault();
@@ -87,7 +109,29 @@ function handleNewCommentSubmit(event) {
   }
 
   const formData = { commentBody, writtenBy };
-}
+
+  fetch(`/api/comments/${pizzaId}`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+    response.json();
+  })
+  .then(commentResponse => {
+    console.log(commentResponse);
+    location.reload();
+  })
+  .catch(err => {
+    console.log(err);
+  });
+};
 
 function handleNewReplySubmit(event) {
   event.preventDefault();
@@ -106,7 +150,27 @@ function handleNewReplySubmit(event) {
   }
 
   const formData = { writtenBy, replyBody };
-}
+
+  fetch(`/api/comments/${pizzaId}/${commentId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+    response.json();
+  })
+  .then(commentResponse => {
+    console.log(commentResponse);
+    location.reload();
+  })
+  .catch(err => console.log(err));
+};
 
 $backBtn.addEventListener('click', function() {
   window.history.back();
@@ -114,3 +178,5 @@ $backBtn.addEventListener('click', function() {
 
 $newCommentForm.addEventListener('submit', handleNewCommentSubmit);
 $commentSection.addEventListener('submit', handleNewReplySubmit);
+
+getPizza();
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controllers');
+const { addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controllers');
 
-// /api/comments/<pizzaId>
+// this route is used to add a comment to a pizza
 router.route('/:pizzaId').post(addComment);
 
-// /api/comments<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+// this route is used to add a reply to a comment or to remove a comment from a pizza
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
+
+// this route is used to remove a reply from a comment
+router
+  .route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
